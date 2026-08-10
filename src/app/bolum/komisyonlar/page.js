796b6a80c5a0c1c578b24f426188bada19b15148
Kaan Necip Kalp <kaannecipk@gmail.com>
2026-08-10 21:17:31 +0300

    feat(bolum): add commissions page

---
+++ src/app/bolum/komisyonlar/page.js
@@ -0,0 +1,10 @@
+import CommissionsPage from "./components/CommissionsPage";
+
+export const metadata = {
+  title: "Komisyonlar",
+  description: "Matematik Mühendisliği Bölümü komisyonları ve üyeleri.",
+};
+
+export default function Page() {
+  return <CommissionsPage />;
+}
